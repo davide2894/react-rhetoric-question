c58e47a058fd08d19126396c58eca475f2d069b0
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-06-02 12:46:30 +0200

    use ternary to determine answer

--- src/components/rhetoricQuestion/RhetoricQuestion.jsx
+++ src/components/rhetoricQuestion/RhetoricQuestion.jsx
@@ -6,21 +6,15 @@ export default function RhetoricQuestion(props) {
      * Challenge: Replace the if/else below with a ternary
      * to determine the text that should display on the page
      */
-    const isGoingOut = true
+    const isGoingOut = true;
 
-    let answer  // Use ternary here
-    if(isGoingOut === true) {
-        answer = "Yes"
-    } else {
-        answer = "No"
-    }
-         
+    //let answer = isGoingOut ? "Yes" : "No";
     
     return(
         <div className="rhetoricQuestion">
             <h1 className="rhetoricQuestion__question">Do I feel like going out tonight?</h1>
             <div className="rhetoricQuestion__answerContainer">
-                <h1 className="rhetoricQuestion__answer">{answer}</h1>
+                <h1 className="rhetoricQuestion__answer">{isGoingOut ? "Yes" : "No"}</h1>
             </div>
         </div>
     )
